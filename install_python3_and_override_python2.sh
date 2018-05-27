9a367ac593d602f5226e984bdf43a62c0d4583dc
Cameron Hudson <CameronHudson8@gmail.com>
2018-05-27 13:58:55 -0700

    Replaced symlink with original copy.

--- install_python3_and_override_python2.sh
+++ install_python3_and_override_python2.sh
@@ -1,1 +1,21 @@
-/Users/cameronhudson/Scripts/install_python3_and_override_python2.sh
+#! /bin/bash
+
+brew update
+brew install python
+
+cp ~/.bash_profile ~./bash_profile.pysave
+
+tee -a ~/.bash_profile << END
+# Setting PATH for Python 3.6
+# The original version is saved in .bash_profile.pysave
+export PATH=$PATH:~/bin
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/usr/local/sbin
+export PATH=$PATH:/usr/local/share/python
+export PATH=$PATH:~/Library/Python/3.6/bin
+alias python='python3'
+alias pip='pip3'
+alias idle='idle3'
+END
+
+source ~/.bash_profile
